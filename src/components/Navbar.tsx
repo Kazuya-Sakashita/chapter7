6cbd5f75acb3cbcd4e9fb9bce7ce5a1f868c9129
Kazuya-Sakashita <kz0508@gmail.com>
2025-02-01 18:27:03 +0900

    課題3】APIで記事データを取得し表示

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="bg-gray-800 text-white w-full">
       <div className="container mx-auto flex justify-between items-center p-6">
-        <h1 className="text-base font-bold">Blog</h1>
+        <Link to="/" className="text-base font-bold">
+          Blog
+        </Link>
         <ul>
           <li className="text-base font-bold">お問い合わせ</li>
         </ul>
--- src/pages/PostList.tsx
+++ src/pages/PostList.tsx
@@ -1,8 +1,49 @@
+import { useEffect, useState } from "react";
 import PostCard from "../components/PostCard";
-import { posts } from "../data/posts";
 import { Post } from "../types/postTypes";
 
 const PostList = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        // データの取得開始
+        const response = await fetch(
+          "https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts"
+        );
+
+        // レスポンスがエラーの場合は例外をスロー
+        if (!response.ok) {
+          throw new Error("データの取得に失敗しました");
+        }
+
+        const data = await response.json();
+        setPosts(data.posts); // データをステートに保存
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message); // エラーメッセージをステートに保存
+        } else {
+          setError("不明なエラーが発生しました"); // エラーが Error インスタンスでない場合の対応
+        }
+      } finally {
+        setLoading(false); // 読み込み中の状態を解除
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  if (loading) {
+    return <div>読み込み中...</div>;
+  }
+
+  if (error) {
+    return <div>エラー: {error}</div>; // エラーメッセージを表示
+  }
+
   return (
     <div>
       <main className="container mx-auto p-4">
